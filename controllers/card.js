1d969e261116f263b55a757969bf90f6902fdad6
nikita <struchk0vnikita@yandex.ru>
2023-10-05 19:15:47 +0300

    четвертая итерация

--- controllers/card.js
+++ controllers/card.js
@@ -13,24 +13,27 @@ module.exports.createCard = (req, res) => {
   console.log(req.user._id); // _id станет доступен
 
   Card.create({ name, link, owner: req.user._id })
-  orFail(new Error('Переданы некорректные данные при создании карточки.'))
     .then((card) => res.status(201).send(card))
     .catch((err) => {
       if (err.name === 'ValidationError') {
-        res.status(400).send({ message: '' });
+        res.status(400).send({ message: 'Переданы некорректные данные при создании карточки.' });
       } else res.status(500).send({ message: 'Внутренняя ошибка сервера' });
     });
 };
 
 //  удаляет карточку по идентификатору
 module.exports.deleteCardById = (req, res) => {
-  Card.findByIdAndRemove(req.params.id)
-  orFail(new Error('Карточка с указанным _id не найдена.'))
+  Card.findByIdAndRemove(req.params.cardId)
+  .orFail(() => Error("NotFound"))
     .then((card) => res.send(card))
     .catch((err) => {
-      if (err.message === 'Карточка с указанным _id не найдена.') {
-        res.status(404).send({ message: '' });
-      } else res.status(500).send({ message: 'Внутренняя ошибка сервера' });
+      if (err instanceof Error && err.message === "NotFound") {
+        res.status(404).send({ message: 'Карточка с указанным _id не найдена.' });
+      }
+      if (err.name === 'CastError') {
+        res.status(400).send({ message: 'Переданы некорректные данные карточки' });
+      }
+      else res.status(500).send({ message: 'Внутренняя ошибка сервера' });
     });
 };
 
@@ -42,14 +45,19 @@ module.exports.likeCard = (req, res) => {
     { $addToSet: { likes: userId } }, // добавить _id в массив, если его там нет
     { new: true },
   )
-  orFail(new Error('Переданы некорректные данные для постановки/снятии лайка'))
+  .orFail(new Error('NotFound'))
     .then((card) => res.send(card))
     .catch((err) => {
-      if (err.message === 'Переданы некорректные данные для постановки/снятии лайка') {
-        res.status(400)
-      } else res.status(500).send({ message: 'Внутренняя ошибка сервера' });
+      if (err.message === 'NotFound') {
+        res.status(404).send({ message: 'Передан несуществующий _id карточки.' });
+      }
+      if (err.name === 'CastError') {
+        res.status(400).send({ message: 'Переданы некорректные данные для постановки/снятии лайка. ' });
+      }
+      else res.status(500).send({ message: 'Внутренняя ошибка сервера' });
     });
 };
+
 
 // убрать лайк с карточки
 module.exports.dislikeCard = (req, res) => {
@@ -57,13 +65,17 @@ module.exports.dislikeCard = (req, res) => {
   Card.findByIdAndUpdate(
     req.params.cardId,
     { $pull: { likes: userId } }, // убрать _id из массива
-    { new: true },
-    orFail(new Error('Передан несуществующий _id карточки.'))
+    { new: true }
   )
+  .orFail(() => Error("NotFound"))
     .then((card) => res.send(card))
     .catch((err) => {
-      if (err.message === 'Передан несуществующий _id карточки.') {
-        res.status(400)
-      } else res.status(500).send({ message: 'Внутренняя ошибка сервера' });
+      if (err instanceof Error && err.message === "NotFound") {
+        res.status(404).send({ message: 'Передан несуществующий _id карточки.' });
+      }
+      if (err.name === 'CastError') {
+        res.status(400).send({ message: 'Переданы некорректные данные для постановки/снятии лайка. ' });
+      }
+      else res.status(500).send({ message: 'Внутренняя ошибка сервера' });
     });
 };
--- controllers/user.js
+++ controllers/user.js
@@ -10,13 +10,17 @@ module.exports.getUsers = (req, res) => {
 // возвращает пользователя по _id
 module.exports.getUsersById = (req, res) => {
   const userId = req.user._id;
-  User.findById(userId);
-  orFail(new Error('Пользователь по указанному _id не найден.'))
-    .then((user) => res.status(200).send({ user }))
+  User.findById(userId)
+  .orFail(new Error('NotFound'))
+    .then((card) => res.send(card))
     .catch((err) => {
-      if (err.message === 'Пользователь по указанному _id не найден.') {
-        res.status(404)
-      } else res.status(500).send({ message: 'Внутренняя ошибка сервера' });
+      if (err.message === 'NotFound') {
+        res.status(404).send({ message: 'Пользователь по указанному _id не найден.' });
+      }
+      if (err.name === 'CastError') {
+        res.status(400).send({ message: 'Переданы некорректные данные' });
+      }
+      else res.status(500).send({ message: 'Внутренняя ошибка сервера' });
     });
 };
 
@@ -25,11 +29,11 @@ module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
 
   User.create({ name, about, avatar })
-   orFail(new Error('Переданы некорректные данные при создании пользователя.'))
+  //  orFail(new Error('Переданы некорректные данные при создании пользователя.'))
     .then((user) => res.status(201).send(user))
     .catch((err) => {
-      if (err.name === 'ValidationError' || err.message === 'Переданы некорректные данные при создании пользователя.') {
-        res.status(400)
+      if (err.name === 'ValidationError') {
+        res.status(400).send({ message: 'Переданы некорректные данные при обновлении профиля.' })
       } else res.status(500).send({ message: 'Внутренняя ошибка сервера' });
     });
 };
@@ -48,26 +52,33 @@ module.exports.updateUser = (req, res) => {
       runValidators: true
     },
   )
-  orFail(new Error('Переданы некорректные данные при обновлении профиля.'))
-    .then((user) => res.send(user))
+  .orFail(new Error('NotFound'))
+    .then((card) => res.send(card))
     .catch((err) => {
-      if (err.name === 'ValidationError' || err.message === 'Переданы некорректные данные при обновлении профиля.') {
-        res.status(400)
-      } else res.status(500).send({ message: 'Внутренняя ошибка сервера' });
+      if (err.message === 'NotFound') {
+        res.status(404).send({ message: 'Пользователь по указанному _id не найден.' });
+      }
+      if (err.name === 'ValidationError' || err.name === 'CastError') {
+        res.status(400).send({ message: 'Переданы некорректные данные при обновлении профиля.' });
+      }
+      else res.status(500).send({ message: 'Внутренняя ошибка сервера' });
     });
 };
-
 // обновляет аватар
 module.exports.updateAvatar = (req, res) => {
   const { avatar } = req.body;
   const userId = req.user._id;
 
   User.findByIdAndUpdate(userId, { avatar }, { new: true,  runValidators: true })
-  orFail(new Error('Переданы некорректные данные при обновлении аватара.'))
-    .then((user) => res.send(user))
+  .orFail(new Error('NotFound'))
+    .then((card) => res.send(card))
     .catch((err) => {
-      if (err.name === 'ValidationError' || err.message === 'Переданы некорректные данные при обновлении аватара.') {
-        res.status(400)
-      } else res.status(500).send({ message: 'Внутренняя ошибка сервера' });
+      if (err.message === 'NotFound') {
+        res.status(404).send({ message: 'Пользователь по указанному _id не найден.' });
+      }
+      if (err.name === 'ValidationError' || err.name === 'CastError') {
+        res.status(400).send({ message: 'Переданы некорректные данные при обновлении аватара.' });
+      }
+      else res.status(500).send({ message: 'Внутренняя ошибка сервера' });
     });
 };
